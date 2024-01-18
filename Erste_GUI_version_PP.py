c4c6a311dfa2acf4c518117419623800c83cfa7e
Enaarian <micail-micon.kruse@data-craft-students.de>
2024-01-18 12:27:42 +0100

    neuer import

--- Erste_GUI_version_PP.py
+++ Erste_GUI_version_PP.py
@@ -2,53 +2,9 @@
 import tkinter as tk
 from tkinter import ttk
 from berechnung.berechnung_main import berechne_immobilienpreis
-
-ausstattung_kostenfaktor = {
-    "Rohbau": 0.5,
-    "Sanierungsbedarf": 0.8,
-    "Renovierungsbedarf": 0.9,
-    "Einfach": 1,
-    "Gehoben": 2
-}
-
-bundesland_kostenfaktoren = {
-    "Baden-Württemberg": 1.5,
-    "Bayern": 1.7,
-    "Berlin": 2.1,
-    "Brandenburg": 1.1,
-    "Bremen": 1.2,
-    "Hamburg": 2.5,
-    "Hessen": 1.3,
-    "Mecklenburg-Vorpommern": 0.9,
-    "Niedersachsen": 1,
-    "Nordrhein-Westfalen": 1.1,
-    "Rheinland-Pfalz": 1,
-    "Saarland": 0.7,
-    "Sachsen": 0.7,
-    "Sachsen-Anhalt": 0.6,
-    "Schleswig-Holstein": 1.4,
-    "Thüringen": 0.6
-}
-
-hausart_kostenfaktor = {
-    "Einfamilienhaus": 1,
-    "Doppelhaushälfte": 0.8,
-    "Mehrfamilienhaus": 0.7
-}
-
-stadt_vs_land_kostenfaktor = {
-    "Land": 1,
-    "Stadt": 2
-}
-
-weitere_kostenfaktoren = {
-    "QM-Grundstück": 160,
-    "QM-Wohnfläche": 2500,
-    "Geplant von Architekt": 20,
-    "Makler": 20,
-    "Denkmalschutz": -25,
-    "Baujahr": -0.1
-}
+from berechnung.kostenfaktoren import (ausstattung_kostenfaktor,
+                            bundesland_kostenfaktoren, hausart_kostenfaktor,
+                            stadt_vs_land_kostenfaktor, weitere_kostenfaktoren)
 
 def geschaetz():
     grundstuecksflaeche_val = grundstuecksflaeche_entry.get()
