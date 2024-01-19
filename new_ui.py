82b1fc6eb4d54e026bb2c779e31702443fc1104a
Enaarian <micail-micon.kruse@data-craft-students.de>
2024-01-19 14:18:31 +0100

    remove comments

--- new_ui.py
+++ new_ui.py
@@ -42,9 +42,7 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
-        # create main entry and button
-        # self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        # self.entry.grid(row=3, column=1, columnspan=1, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        # create main button
 
         self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
                                                      text_color=("gray10", "#DCE4EE"),
@@ -60,7 +58,6 @@ class App(customtkinter.CTk):
         self.tabview.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.tabview.add("Daten")
         # self.tabview.add("Tab 2")
-        # self.tabview.add("Tab 3")
         self.tabview.tab("Daten").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
         self.tabview.tab("Daten").grid_columnconfigure(0, weight=1)
 
@@ -88,14 +85,6 @@ class App(customtkinter.CTk):
         self.entry_gf1.grid(row=0, column=2, columnspan=1, padx=(20, 0), pady=(0, 20), sticky="nsew")
         self.entry_bau = customtkinter.CTkEntry(self.tabview.tab("Daten"), placeholder_text="Baujahr")
         self.entry_bau.grid(row=2, column=2, columnspan=1, padx=(20, 0), pady=(0, 20), sticky="nsew")
-        # self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
-        #                                             values=["Value 1", "Value 2", "Value Long....."])
-        # self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("CTkTabview"), text="Open CTkInputDialog",
-        #                                                    command=self.open_input_dialog_event)
-        # self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        # self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
-        # self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
         self.checkbox_1 = customtkinter.CTkCheckBox(self.tabview.tab("Daten"), text="Architektenhaus")
         self.checkbox_1.grid(row=0, column=4, pady=(0, 20), padx=20, sticky="nsew")
         self.checkbox_2 = customtkinter.CTkCheckBox(self.tabview.tab("Daten"), text="Makler Verkauf")
@@ -107,81 +96,15 @@ class App(customtkinter.CTk):
         self.textbox_result = customtkinter.CTkTextbox(self, width=80, border_width=2)
         self.textbox_result.grid(row=1, column=3, padx=(20, 20), pady=(40, 0), sticky="nsew")
 
-        # create radiobutton frame
-        # self.radiobutton_frame = customtkinter.CTkFrame(self)
-        # self.radiobutton_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        # self.radio_var = tkinter.IntVar(value=0)
-        # self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
-        # self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
-        # self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-        # value=0)
-        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-        # value=1)
-        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var,
-        # value=2)
-        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
-
-        # # create slider and progressbar frame
-        # self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-        # self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        # self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-        # self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
-        # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-        # self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-        # self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-        # self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
-
-        # create scrollable frame
-        # self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
-        # self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        # self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        # self.scrollable_frame_switches = []
-        # for i in range(3):
-        #     switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i+1}")
-        #     switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-        #     self.scrollable_frame_switches.append(switch)
-
-        # create checkbox and switch frame
-        # self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        # self.checkbox_slider_frame.grid(row=0, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        # self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        # self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        # self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        # self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
-
         # set default values
-
-        # self.checkbox_3.configure(state="disabled")
-        # self.checkbox_1.select()
-        # self.scrollable_frame_switches[0].select()
-        # self.scrollable_frame_switches[1].select()
-        # self.radio_button_3.configure(state="disabled")
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
         self.optionmenu_1.set("Berlin")
         self.optionmenu_2.set("Land")
         self.optionmenu_3.set("Einfach")
         self.optionmenu_4.set("Einfamilienhaus")
-        # self.combobox_1.set("CTkComboBox")
-        # self.slider_1.configure(command=self.progressbar_2.set)
-        # self.slider_2.configure(command=self.progressbar_3.set)
-        # self.progressbar_1.configure(mode="indeterminnate")
-        # self.progressbar_1.start()
         self.textbox.insert("0.0", "CTkTextbox\n\n" + "Hallo Welt")
-        # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        # self.seg_button_1.set("Value 2")
+
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
@@ -209,10 +132,10 @@ class App(customtkinter.CTk):
             self.textbox_result = customtkinter.CTkTextbox(self, width=80, border_width=2, text_color="red")
             self.textbox_result.grid(row=1, column=3, padx=(20, 20), pady=(40, 0), sticky="nsew")
             self.textbox_result.insert("0.0",
-                text="-!!!Ungültige Eingabe.\n Stelle sicher,"
-                     " dass Zahlen korrekt eingegeben wurden.!!!\n")
-
+                text="!!!Ungültige Eingabe.\n Stelle sicher,\n"
+                     " dass Zahlen korrekt\n eingegeben wurden!!!\n")
             return
+
         self.textbox_result = customtkinter.CTkTextbox(self, width=80, border_width=2)
         self.textbox_result.grid(row=1, column=3, padx=(20, 20), pady=(40, 0), sticky="nsew")
         gf = float(self.entry_gf1.get())
@@ -225,7 +148,6 @@ class App(customtkinter.CTk):
         ausstattung = self.optionmenu_3.get()
         hausart = self.optionmenu_4.get()
         bundesland = self.optionmenu_1.get()
-
         preis = berechne_immobilienpreis(gf, wf, architektenhaus, makler,
                                          denkmalschutz, baujahr, lage, ausstattung,
                                          hausart, bundesland)
@@ -236,8 +158,7 @@ class App(customtkinter.CTk):
         .replace(
             "X", "."))
         self.textbox_result.insert("0.0", text=f"-Der überteuerte Schätzpreis:\n{preis_text}\n")
-    # def error_redtext(self,text_color,text_field):
-    #     if
+
 
 if __name__ == "__main__":
     app = App()
